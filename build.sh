be24623e2a550d1fb75114c5fb819fa3a235d2a3
Robert Lemmens <rj.lemmens@pm.me>
2024-03-20 19:05:18 +0100

    pinned build version to a compatible jvm / platform

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -it --rm -v $PWD:/app -w /app mozilla/sbt sbt "project backend" assembly
+docker run -it --rm -v $PWD:/app -w /app mozilla/sbt:8u292_1.5.7 sbt "project backend" assembly
 docker build -t rlemmens/youtube-sync:latest .
